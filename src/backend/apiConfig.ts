bc552de0ce14d3f094e2d6989e056a86b3279f45
Mauricio Klainbard <136375869+MDKMauricioKlainbard@users.noreply.github.com>
2024-01-28 00:10:59 -0300

    Update apiConfig.ts

--- src/backend/apiConfig.ts
+++ src/backend/apiConfig.ts
@@ -1,4 +1,4 @@
-export const API_URL: string = 'http://localhost:3000';
+export const API_URL: string = 'https://nft-dapp-golden-elf-back-production.up.railway.app';
 export const API_KEY: string = 'SECRET_KEY';
 
 //The API_KEY constant should be a hidden constant.
