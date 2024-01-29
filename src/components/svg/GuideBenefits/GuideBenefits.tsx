961ad11846df874dfefab058489cc1be9c7f1c30
MDKMauricioKlainbard <mauricioklainbard01@gmail.com>
2024-01-28 23:52:12 -0300

    Hotfix svg

--- src/components/svg/GuideBenefits/GuideBenefits.tsx
+++ src/components/svg/GuideBenefits/GuideBenefits.tsx
@@ -3,9 +3,7 @@ import styles from './GuideBenefits.module.css'
 
 const GuideBenefits: FC = () => {
     return (
-        <svg id="Capa_2" data-name="Capa 2"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 791.51 335.62">
+        <svg id="Capa_2" data-name="Capa 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 798.25 302.49">
             <defs>
                 <filter id="outer-glow-1" filterUnits="userSpaceOnUse">
                     <feOffset dx="0" dy="0" />
@@ -28,15 +26,31 @@ const GuideBenefits: FC = () => {
                     <feComposite in2="blur-3" operator="in" />
                     <feComposite in="SourceGraphic" />
                 </filter>
+                <filter id="outer-glow-4" filterUnits="userSpaceOnUse">
+                    <feOffset dx="0" dy="0" />
+                    <feGaussianBlur result="blur-4" stdDeviation="3" />
+                    <feFlood floodColor="#fff" floodOpacity="1" />
+                    <feComposite in2="blur-4" operator="in" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+                <filter id="outer-glow-5" filterUnits="userSpaceOnUse">
+                    <feOffset dx="0" dy="0" />
+                    <feGaussianBlur result="blur-5" stdDeviation="3" />
+                    <feFlood floodColor="#fff" floodOpacity="1" />
+                    <feComposite in2="blur-5" operator="in" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
             </defs>
             <g id="_3" data-name="3">
-                <polygon className={styles["cls-1"]} points="418.48 263.29 427.14 254.29 428.03 266.78 440.47 265.49 433.13 275.6 443.52 282.61 431.38 285.6 434.86 297.64 423.61 292.12 418.56 303.55 413.46 292.1 402.23 297.57 405.66 285.55 393.52 282.51 403.88 275.54 396.5 265.4 408.94 266.74 409.78 254.26 418.48 263.29" />
-                <polygon className={styles["cls-3"]} points="54.9 226.26 63.57 217.27 64.46 229.76 76.89 228.47 69.55 238.58 79.94 245.59 67.81 248.58 71.29 260.62 60.04 255.1 54.98 266.53 49.88 255.08 38.65 260.55 42.09 248.53 29.94 245.49 40.3 238.52 32.92 228.38 45.36 229.72 46.2 217.24 54.9 226.26" />
-                <text className={styles["cls-2"]} transform="translate(79.94 20.41)"><tspan className={styles["cls-5"]} x="0" y="0">Staking y </tspan><tspan className={styles["cls-4"]} x="122.4" y="0">T</tspan><tspan className={styles["cls-5"]} x="135.89" y="0">o</tspan><tspan className={styles["cls-8"]} x="151.49" y="0">k</tspan><tspan className={styles["cls-13"]} x="166.29" y="0">enomics</tspan><tspan className={styles["cls-5"]} x="0" y="28.8">Ganar </tspan><tspan className={styles["cls-4"]} x="80.23" y="28.8">T</tspan><tspan className={styles["cls-5"]} x="93.72" y="28.8">o</tspan><tspan className={styles["cls-12"]} x="109.32" y="28.8">k</tspan><tspan className={styles["cls-14"]} x="124.13" y="28.8">ens NGold</tspan></text>
-                <text className={styles["cls-2"]} transform="translate(344.88 141.54)"><tspan className={styles["cls-5"]} x="0" y="0">Suscripciones GR</tspan><tspan className={styles["cls-9"]} x="216.14" y="0">A</tspan><tspan className={styles["cls-13"]} x="233.18" y="0">TIS en NNchat</tspan><tspan className={styles["cls-14"]} x="0" y="28.8">Usuarios </tspan><tspan className="cls-6" x="114.21" y="28.8">C</tspan><tspan className={styles["cls-13"]} x="130.82" y="28.8">ertifi</tspan><tspan className="cls-11" x="192.36" y="28.8">c</tspan><tspan className={styles["cls-5"]} x="205.92" y="28.8">ados</tspan></text>
-                <text className={styles["cls-2"]} transform="translate(458.87 291.15)"><tspan x="0" y="0">C</tspan><tspan className={styles["cls-5"]} x="16.61" y="0">ómic sobre los Golden Elf</tspan></text>
-                <text className={styles["cls-2"]} transform="translate(.12 295.98)"><tspan className={styles["cls-5"]} x="0" y="0">Jugar pa</tspan><tspan className={styles["cls-7"]} x="107.69" y="0">r</tspan><tspan className={styles["cls-5"]} x="117.93" y="0">a ganar la</tspan><tspan className={styles["cls-5"]} x="0" y="28.8">última gene</tspan><tspan className={styles["cls-15"]} x="150.19" y="28.8">r</tspan><tspan className={styles["cls-5"]} x="160.44" y="28.8">ación</tspan></text>
-                <path className={styles["cls-10"]} d="m71.05,63.15s238.98,87.83,234.89,86.3c-4.09-1.53-252.77,93.96-252.77,93.96l365.62,36.77" />
+                <polygon className={styles["cls-3"]} points="70.16 18.41 78.82 9.42 79.71 21.91 92.15 20.62 84.81 30.72 95.2 37.73 83.07 40.73 86.54 52.76 75.3 47.25 70.24 58.67 65.14 47.23 53.91 52.7 57.34 40.68 45.2 37.63 55.56 30.66 48.18 20.52 60.62 21.87 61.46 9.38 70.16 18.41" />
+                <polygon className={styles["cls-7"]} points="309.56 100.71 318.23 91.72 319.12 104.2 331.55 102.91 324.21 113.02 334.6 120.03 322.47 123.03 325.95 135.06 314.7 129.54 309.64 140.97 304.54 129.52 293.31 134.99 296.75 122.97 284.6 119.93 294.96 112.96 287.58 102.82 300.02 104.16 300.86 91.68 309.56 100.71" />
+                <polygon className={styles["cls-5"]} points="425.22 232.28 433.89 223.29 434.78 235.78 447.21 234.49 439.87 244.59 450.26 251.61 438.13 254.6 441.61 266.64 430.36 261.12 425.3 272.55 420.2 261.1 408.97 266.57 412.41 254.55 400.26 251.5 410.62 244.53 403.24 234.4 415.68 235.74 416.52 223.25 425.22 232.28" />
+                <polygon className={styles["cls-6"]} points="61.65 195.26 70.31 186.27 71.2 198.76 83.64 197.47 76.3 207.57 86.69 214.58 74.55 217.58 78.03 229.62 66.79 224.1 61.73 235.52 56.63 224.08 45.4 229.55 48.83 217.53 36.69 214.48 47.05 207.51 39.67 197.38 52.11 198.72 52.95 186.23 61.65 195.26" />
+                <text className={styles["cls-8"]} transform="translate(115.07 26.53)"><tspan className={styles["cls-12"]}><tspan className={styles["cls-10"]} x="0" y="0">Staking y </tspan><tspan className={styles["cls-9"]} x="122.4" y="0">T</tspan><tspan className={styles["cls-10"]} x="135.89" y="0">o</tspan><tspan className={styles["cls-14"]} x="151.49" y="0">k</tspan><tspan className={styles["cls-18"]} x="166.29" y="0">enomics</tspan></tspan><tspan className={styles["cls-23"]} x="0" y="28.8">Ganar Tokens NGold</tspan></text>
+                <text className={styles["cls-8"]} transform="translate(351.24 110.53)"><tspan className={styles["cls-12"]}><tspan className={styles["cls-10"]} x="0" y="0">Suscripciones GR</tspan><tspan className={styles["cls-15"]} x="216.14" y="0">A</tspan><tspan className={styles["cls-18"]} x="233.18" y="0">TIS en NNchat</tspan></tspan><tspan className={styles["cls-24"]} x="0" y="28.8">Usuarios certificados</tspan></text>
+                <text className={styles["cls-4"]} transform="translate(465.62 260.15)"><tspan x="0" y="0">C</tspan><tspan className={styles["cls-10"]} x="16.61" y="0">ómic sobre los Golden Elf</tspan></text>
+                <text className={styles["cls-4"]} transform="translate(.12 262.84)"><tspan className={styles["cls-10"]} x="0" y="0">Jugar pa</tspan><tspan className={styles["cls-13"]} x="107.69" y="0">r</tspan><tspan className={styles["cls-10"]} x="117.93" y="0">a ganar la</tspan><tspan className={styles["cls-10"]} x="0" y="28.8">última gene</tspan><tspan className={styles["cls-22"]} x="150.19" y="28.8">r</tspan><tspan className={styles["cls-10"]} x="160.44" y="28.8">ación</tspan></text>
+                <path className={styles["cls-16"]} d="m77.79,32.15s238.98,87.83,234.89,86.3c-4.09-1.53-252.77,93.96-252.77,93.96l365.62,36.77" />
             </g>
         </svg>
     )
